fd53dd83e107ba164dac771f4ef6e41550e3ee03
pimentahodl <pimenta@hodlfinance.com>
2019-01-20 23:53:15 +0000

    log conent

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // set the port of our application
@@ -6,29 +6,42 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the home page route
-app.get('/', function(req, res) {
-	res.sendFile(__dirname+"/index.html");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/results', function(req, res) {
-	res.sendFile(__dirname+"/results.html");
+app.get("/results", function(req, res) {
+  res.sendFile(__dirname + "/results.html");
 });
-
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+  console.log("Our app is running on http://localhost:" + port);
 });
 
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
-  await page.goto('https://polishignorant.github.io/');
-  setInterval(async ()=>{
-	  const content = await page.$eval('#text', e => e.innerHTML);
-	  fs.writeFile(__dirname+"/results.html", content, (err) => {
-	  });
+  let page = null;
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    page = await browser.newPage();
+    await page.goto("https://polishignorant.github.io/");
+  } catch (e) {
+    console.log(e);
+  }
+
+  setInterval(async () => {
+    if (!page) {
+      console.log("no page");
+      return;
+    }
+    const content = await page.$eval("#text", e => e.innerHTML);
+    console.log(content);
+    fs.writeFile(__dirname + "/results.html", content, err => {
+		if(err) console.log("err")
+	});
   }, 10000);
 })();
