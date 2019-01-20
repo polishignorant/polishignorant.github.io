8805ff9f264ad19571b167b8ee0f4dcd7b7c7b2c
pimentahodl <pimenta@hodlfinance.com>
2019-01-20 23:22:59 +0000

    server

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the home page route
+app.get('/', function(req, res) {
+	res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
