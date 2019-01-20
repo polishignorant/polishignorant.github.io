1122992eaa6a7db403eed8b05056a6d4d36449d5
pimentahodl <pimenta@hodlfinance.com>
2019-01-20 23:49:10 +0000

    pupeteer config

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://polishignorant.github.io/');
   setInterval(async ()=>{
